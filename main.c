97e400d02f3a0579e9c4950f2b21d494a2be942d
Loic Andre <loc_andre@yahoo.fr>
2020-10-10 12:08:09 +0200

    Code rework

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
-//#include <pic12f1572.h>
+////////////////////////////////////////////////////////////////////////////////
+// LED_PWM_Controller                                                         //
+// Platform : PIC12F1572                                                      //
+// Compiler : XC 2.10, IDE : MPLAB X V5.35, PicKit3                           //
+//                                                                            //
+// Loic Andre                                                                 //
+// Creative Commons CC-BY-NC                                                  //
+////////////////////////////////////////////////////////////////////////////////
+
+
 #include <stdio.h>
 #include <xc.h>
-
 
 // CONFIG1
 #pragma config FOSC = INTOSC    //  (INTOSC oscillator; I/O function on CLKIN pin)
 #pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
 #pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
-#pragma config MCLRE = ON      // MCLR Pin Function Select (MCLR/VPP pin function is digital input)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is digital input)
 #pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
 #pragma config BOREN = OFF      // Brown-out Reset Enable (Brown-out Reset disabled)
 #pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
@@ -15,7 +23,7 @@
 // CONFIG2
 #pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
 #pragma config PLLEN = OFF      // PLL Enable (4x PLL disabled)
-#pragma config STVREN = ON     // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will not cause a Reset)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will not cause a Reset)
 #pragma config BORV = HI        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), high trip point selected.)
 #pragma config LPBOREN = OFF    // Low Power Brown-out Reset enable bit (LPBOR is disabled)
 #pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
@@ -29,12 +37,11 @@
 
 #define _ADDR 1 
 
-#define _VIN_UNDERVOLTAGE_DEAD 1000
-#define _VIN_UNDERVOLTAGE_MIN 1066
-#define _VIN_UNDERVOLTAGE_MAX 1122
-#define _PMAXOFFSET 20 // Potentiometre max offset
+#define _VIN_UNDERVOLTAGE_CUTOFF 1000 // with error correction >  10.25 V
+#define _VIN_UNDERVOLTAGE_MIN 1066    // with error correction >  11.00 V
+#define _VIN_UNDERVOLTAGE_MAX 1122    // with error correction >  11.50 V
+#define _PMAXOFFSET 20 // Potentiometer max offset
 #define _PMINOFFSET 90
-#define _FILTER_WINDOW_SIZE 50
 
 #define ADC_CHANNEL_POT 0
 #define ADC_CHANNEL_VIN 1
@@ -42,23 +49,18 @@
 char LOW_VOLTAGE =0;
 char UPDATE_JOB =0;
 char UART_MODE =0;
+int UART_MODE_last_POT =0;
 
 long VIN_Value=0;
 long VIN_Value_filter=_VIN_UNDERVOLTAGE_MIN;
 long LED_Value=0;
 int  POT_Value=0;
-long  POT_Value_last=0;
+long POT_Value_filter=1024;
     
 int RX_VALUE =0;
 unsigned char RX_CHEKSUM =0;
 int RX_VALUE_temp =0;
 char RX_DECODE_SEQ =0;
-
-//int FILTER_INDEX =0;
-//long FILTER_SUM =0;
-//int FILTER_SAMPLES[_FILTER_WINDOW_SIZE];
-//int FILTER_AVERAGED =0;
-int FILTER_AVERAGED_last =0;
 
 int ADC_result(unsigned char channel)
 {
@@ -80,7 +82,6 @@ void PWM()
     PWM3CON = 0b01100000;             // PWM control (standard mode).
     PWM3PH = 0;                       // Phase.
     PWM3PR = 1400;                    // Period. (ADC is max 1024, 13.6v-12v = 1.33 * 1024=1160 -> Duty cycle max is 88%)
-    //APFCONbits.P1SEL = 1;           // PWM1 on RA5.
     PWM3DC =0;
     PWMLDbits.PWM3LDA_A=1; // trig new value
     PWMENbits.PWM3EN_A=1;
@@ -92,8 +93,8 @@ void ADC_set()
     FVRCONbits.ADFVR = 0b10;        // ADC FVR gain is 2 --> 2x1.024V
     
     ANSELAbits.ANSA4=1;             // Analog input on PIN RA4 // ADC_POT
-    ANSELAbits.ANSA1=1;             // Analog input on PIN RA1 // ADC_VIN
-    //ADCON0bits.CHS=0b11;            // Analog channel RA4
+    ANSELAbits.ANSA1=1;             // Analog input on PIN RA1 // ADC_VIN 
+    
     ADCON1bits.ADFM=1;              // ADC set to ADRESL
     ADCON0bits.ADON=1;              // ADC enabled
     ADCON1bits.ADCS=0b11;           // ADC conversion clock select as FRC
@@ -102,13 +103,14 @@ void ADC_set()
 
 void Timer2init()
 {
-    T2CONbits.T2OUTPS = 0b11;   // 64bits prescaler
-    T2CONbits.T2OUTPS = 0b1111; // 16bits postscaler
+    T2CONbits.T2CKPS = 0b10;   // 16bits prescaler
+    T2CONbits.T2OUTPS = 0b0000; // no postscaler
     
-    TMR2 = 255;
-    T2CONbits.TMR2ON = 1; // 16bits postscale
-    PIE1bits.TMR2IE = 1; // enable timer 2 interrupt
+    PR2 = 250; // --> 32'000'000Mhz / 4 / prescaler (16) / PR2 (250)  = 2Khz
+    TMR2 = 0;
+    T2CONbits.TMR2ON = 1; // Enable timer
     PIR1bits.TMR2IF = 0; // Reset timer 2 interrupt
+    PIE1bits.TMR2IE = 1; // Enable timer 2 interrupt
 }
 
 void UARTinit()
@@ -131,7 +133,7 @@ void main(void)
 {
    
     // init GPIO
-    TRISAbits.TRISA0=1;             // I/O RA0 Input   // NC
+    TRISAbits.TRISA0=1;             // I/O RA0 Input    // NC
     WPUAbits.WPUA0=1;               // RA0 Input pull-up
     TRISAbits.TRISA1=1;             // I/O RA1 Input    // ADC_VIN
     TRISAbits.TRISA2=0;             // I/O RA2 Ouptut   // PWM_OUT
@@ -148,31 +150,32 @@ void main(void)
     INTCONbits.GIE =1; // global interrupt enable
     INTCONbits.PEIE =1; // Peripherals interrupt enable
     
-    // init Moving averaging Filter
-    //for(FILTER_INDEX=0; FILTER_INDEX<= _FILTER_WINDOW_SIZE; FILTER_INDEX++){
-    //    FILTER_SAMPLES[FILTER_INDEX]=0;
-    //}
-    //FILTER_INDEX =0;
-    //FILTER_SUM =0;
-    
     //init
     ADC_set();
     PWM();
     Timer2init();
     UARTinit();
     
+    // wait 100 ms to be sure ( 2Khz / 200 => 100ms)
+    while(UPDATE_JOB >= 200){ 
+        UPDATE_JOB == 0;
+    }
+    
     while(1)
     {
-        if(UPDATE_JOB > 12)
+        if(UPDATE_JOB > 10) //  2kHz / 10 = 200 Hz
         {
+            // VIN Measure and filter
             VIN_Value_filter = ((VIN_Value_filter *31) + ADC_result(ADC_CHANNEL_VIN) + ADC_result(ADC_CHANNEL_VIN)) / 33;
             VIN_Value = (((VIN_Value_filter*2) * 725) / 1000); // VIN_Value = 1024 * 2(=2048v) * (14.85v/2.048v)->7.25->725
             
-            if(VIN_Value <= _VIN_UNDERVOLTAGE_DEAD){ // 10.250V
-                INTCONbits.GIE =0; // global interrupt enable
-                INTCONbits.PEIE =0; // Peripherals interrupt enable
-                PORTAbits.RA2=0;                // SET PWM_OUT to 0b
-                SLEEP();
+            // Undervoltage Tests
+            if(VIN_Value <= _VIN_UNDERVOLTAGE_CUTOFF){ // 10.250V
+                //INTCONbits.GIE =0; // global interrupt disable
+                //INTCONbits.PEIE =0; // Peripherals interrupt disable
+                //PORTAbits.RA2=0;                // SET PWM_OUT to 0ff
+                //SLEEP(); // no wakeup possible than powercycle / reset
+                LOW_VOLTAGE = 2;
             }
             else  if(VIN_Value <= _VIN_UNDERVOLTAGE_MIN){ // 11.00V
                 LOW_VOLTAGE = 1;
@@ -182,18 +185,20 @@ void main(void)
             }
             
                 
-            POT_Value= (long)((((long)POT_Value_last *62) + ADC_result(ADC_CHANNEL_POT) + ADC_result(ADC_CHANNEL_POT)) / 64);
+            // POT Measure and filter
+            POT_Value= (long)((((long)POT_Value_filter *62) + ADC_result(ADC_CHANNEL_POT) + ADC_result(ADC_CHANNEL_POT)) / 64);
+            POT_Value_filter = POT_Value;
             
-            POT_Value_last = POT_Value;
-            
-            //POT_Value= ADC_result(ADC_CHANNEL_POT);
+            // POT remap and filter
             POT_Value = 1024 - (long)POT_Value - (long)_PMAXOFFSET;    // return Value because POTmax = 0 and POTmin = ~1024
             if(POT_Value < _PMINOFFSET)       // make sure than no minus value is there (due to _PMAXOFFSET)
                 POT_Value = _PMINOFFSET;
 
-            if(UART_MODE == 1) // UART MODE
+            
+            // UART MODE (UART is prioritary ! To quit uart mode, stop UART frames, and turn the potentiometer)
+            if(UART_MODE == 1) 
             {
-                int calc = FILTER_AVERAGED_last;
+                int calc = UART_MODE_last_POT;
 
                 calc -= POT_Value ;
                 if ((calc > 50) ||(calc < -50))
@@ -206,26 +211,21 @@ void main(void)
             }
             else
             {
-                FILTER_AVERAGED_last = POT_Value;
+                UART_MODE_last_POT = POT_Value;
             }
 
-            //Moving Averaging Filter
-            /*FILTER_SUM -= FILTER_SAMPLES[FILTER_INDEX]; // remove oldest entry
-            FILTER_SAMPLES[FILTER_INDEX] = POT_Value;     // add new entry
-            FILTER_SUM += POT_Value;                      // Sum new value
-            FILTER_INDEX = (FILTER_INDEX+1) % _FILTER_WINDOW_SIZE;   // Increment the index, and wrap to 0 if it exceeds the window size
-            FILTER_AVERAGED = FILTER_SUM / _FILTER_WINDOW_SIZE;      // Divide the sum of the window by the window size for the result
-            */
-            
-            
+            // y=f(x)=(x^2)/2048 Exponential remap for better illumination control
             LED_Value = POT_Value;
-
-            //LED_Value = FILTER_AVERAGED;
-            LED_Value = LED_Value*LED_Value; // y=f(x)=(x^2)/2048 Exponential remap
+            LED_Value = LED_Value*LED_Value;
             LED_Value = (LED_Value >> 10);
             
             
-            if(LOW_VOLTAGE == 1){  //in case of LOW_VOLTAGE, PWM = 7% max
+            //in case of LOW_VOLTAGE cutoff, PWM = off
+            if(LOW_VOLTAGE == 2){
+                PWM3DC = 0;
+            }
+            //in case of LOW_VOLTAGE, PWM = 7% max
+            else if(LOW_VOLTAGE == 1){
                 if(LED_Value <= 100){
                     PWM3DC = LED_Value;
                 }
@@ -246,9 +246,10 @@ void main(void)
 void __interrupt() ISR()
 {    
     if (PIR1bits.TMR2IF){
+        // interruption every 2kHz
         UPDATE_JOB ++;
         
-        PIR1bits.TMR2IF = 0;
+        PIR1bits.TMR2IF = 0; // reset interrupt flag
     } 
     
     if(PIR1bits.RCIF){
@@ -286,29 +287,7 @@ void __interrupt() ISR()
             RX_DECODE_SEQ =0;
         }
         
-        PIR1bits.RCIF  =0;
+        PIR1bits.RCIF  =0; // reset interrupt flag
     }
 }
 
-
-
-
-            
-//            POT_Value= ADC_result();
-//        
-//            //Moving Averaging Filter
-//            FILTER_SUM -= FILTER_SAMPLES[FILTER_INDEX]; // remove oldest entry
-//            FILTER_SAMPLES[FILTER_INDEX] = POT_Value;     // add new entry
-//            FILTER_SUM += POT_Value;                      // Sum new value
-//            FILTER_INDEX = (FILTER_INDEX+1) % _FILTER_WINDOW_SIZE;   // Increment the index, and wrap to 0 if it exceeds the window size
-//            FILTER_AVERAGED = FILTER_SUM / _FILTER_WINDOW_SIZE;      // Divide the sum of the window by the window size for the result
-//
-//            LED_Value = 1024 - (long)FILTER_AVERAGED - (long)_PMAXOFFSET;    // return Value because POTmax = 0 and POTmin = ~1024
-//            if(LED_Value < _PMINOFFSET)       // make sure than no minus value is there (due to _PMAXOFFSET)
-//                LED_Value = _PMINOFFSET;
-//            LED_Value = LED_Value*LED_Value; // y=f(x)=(x^2)/2048 Exponantiel remap
-//            LED_Value = (LED_Value >> 11);
-//
-//            PWM3DC = LED_Value;
-//            //PWM3DC=(ADRES + 10) >> 1;
-//            PWMLDbits.PWM3LDA_A=1; // trig new value
